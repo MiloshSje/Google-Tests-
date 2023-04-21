b78a7341a490b940803055de0c37b111620802a6
MiloshSje <milosh.sjeklokja@lodgify.com>
2023-04-21 14:42:26 +0200

    first commit

---
+++ cypress/e2e/googletest.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://google.com')
+    cy.get('.gLFyf').type('cypress{enter}');
+    cy.get('a[href="https://www.cypress.io/"]').should('exist')
+
+  })
+})
